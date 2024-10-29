9144799b8a2489db9f57e57abd439c20875a8917
Nimira43 <n.rabone72@aol.com>
2024-10-29 16:20:01 +0000

    Using notes.md to archive old code snippets I want to keep. Brought in AuthScreen to Home page. Also cleaned up some files.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,9 @@
-import React from 'react'
-import { Button } from '@/components/ui/button'
+import { AuthScreen } from "@/features/auth/components/auth-screen"
 
-export default function page() {
+export default function Home() {
   return (
-    <>
-      <div className='bg-[#111] text-[#87ceeb] p-10 border-b-8 border-[#ffa500]'>
-      <h1 className='logo'>Synergasia</h1>
-      <p>by NimiraTech</p>
-      <h4>Technologies</h4>
-      <h5>Next JS</h5>
-      <h5>TypeScript</h5>
-      <h5>Tailwind</h5>
-      <h5>Shadcn-UI</h5>
+    <div>
+      <AuthScreen />
     </div>
-    <div className='bg-[#87ceed] text-[#111] p-10 border-b-8 border-[#ffa500]'>
-      <Button variant='synergasia'>Button</Button>
-    </div>
-    </>
-    
   )
 }
