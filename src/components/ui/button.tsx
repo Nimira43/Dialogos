2a6bf9fdb8f18956ada87a8e0ca8a627e53462a6
Nimira43 <n.rabone72@aol.com>
2024-10-29 15:53:48 +0000

    Changed Shadcn theme to orange

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,7 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        synergasia: "text-[#ff4500] bg-[#111] hover:bg-[#111]/90 hover:text-[#87ceeb]",
+        synergasia: "text-[#ffa500] bg-[#111] hover:bg-[#111]/90 hover:text-[#87ceeb]",
       },
       size: {
         default: "h-10 px-4 py-2",
