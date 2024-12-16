9cfe961c9f095b226789722850922ac0839140fa
Nimira43 <n.rabone72@aol.com>
2024-12-16 15:57:21 +0000

    Shadcn brought in.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
@@ -6,6 +7,7 @@ const page = () => {
       <h1 className='logo'>Diálogos</h1>
       <h1 className='heading'>Heading</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, exercitationem?</p>
+      <Button>Read More</Button>
     </div>
   )
 }
