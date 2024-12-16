d3ea0040147da57db03d3a9a4abb9ce53204b08d
Nimira43 <n.rabone72@aol.com>
2024-12-16 17:16:30 +0000

    Included an alternative script for the middleware.ts - just in case.

--- middleware.ts
+++ middleware.ts
@@ -10,9 +10,23 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
+
+// Alternative code
+
+// import { clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server'; 
+ 
+// const isProtectedRoute = createRouteMatcher([ '/(.*)', ]);
+// const isPublicRoute = createRouteMatcher(['/api/uploadthing']) 
+ 
+// export default clerkMiddleware(async (auth, req) =>
+//     { 
+//         if (isPublicRoute(req)) return;
+//         if (isProtectedRoute(req)) await auth.protect();
+//     }
+// ); 
+ 
+// export const config = { matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],};
