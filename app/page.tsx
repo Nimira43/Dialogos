962551a729a039af5fb96f047ffe6d55449e8be6
Nimira43 <n.rabone72@aol.com>
2024-12-31 09:21:29 +0000

    Brought in Shadcn and set theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <div>
       <h1 className='logo'>Dialogos</h1>
+      <hr />
+      <Button>Login</Button>
+      <Button variant='outline'>Register</Button>
     </div>
   )
 }
